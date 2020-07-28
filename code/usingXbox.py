e27648b364b83e940c7ac29f1319cbcdac153282
judacas <67698498+judacas@users.noreply.github.com>
2020-07-28 12:13:29 -0400

    Delete usingXbox.py
    Not needed

--- code/usingXbox.py
+++
@@ -1,9 +0,0 @@
-import evdev
-from evdev import InputDevice, categorize, ecodes
-
-#cree un objet gamepad | creates object gamepad
-gamepad = InputDevice('/dev/input/event2')
-
-print(gamepad.active_keys(verbose = False))
-print(gamepad.absinfo(0).max)
-print(gamepad.absinfo(0).min)
